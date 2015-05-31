ffa98b944ea3cd4fe20c36ea676d2f3ca9149bcd
thomazpicelli <thomazpicelli@gmail.com>
2015-05-31 00:06:57 -0300

    inicio criar conta

---
+++ Banco/Banco/Controllers/ContaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Banco.Dados;
+
+namespace Banco.Controllers
+{
+    public class ContaController : Controller
+    {
+        //
+        // GET: /Conta/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Criar()
+        {
+            return View();
+        }
+
+    }
+}
